9700f4fb4c822966fcdc71a681b80943c6bd87ad
Dmitry Egupov <egupovds@gmail.com>
2023-04-29 09:37:24 +0600

    1

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 //::::::::::::::::::::::::::::::::::::::::::::::::::
 //Анимация прокрутки до якоря
 //::::::::::::::::::::::::::::::::::::::::::::::::::
+
+function openTab(tabName, button) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    button.className += " active";
+  }
