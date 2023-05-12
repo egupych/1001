055f2d22732b15eb89f386b5212022e6132f978a
Dmitry Egupov <egupovds@gmail.com>
2023-05-12 07:32:38 +0600

    1

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 //::::::::::::::::::::::::::::::::::::::::::::::::::
-//Анимация прокрутки до якоря
-//::::::::::::::::::::::::::::::::::::::::::::::::::
-
-function openTab(tabName, button) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(tabName).style.display = "flex";
-    button.className += " active";
+function openTab() {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  for (i = 0; i < arguments.length; i++) {
+    document.getElementById(arguments[i]).style.display = "flex";
+  }
+}
 
 window.onload = function() {
   // код для отображения страницы
